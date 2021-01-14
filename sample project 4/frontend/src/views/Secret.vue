b8d509b151a5bff169353df66a9e2803ab839d69
JpJoubert777 <47599182+JpJoubert777@users.noreply.github.com>
2021-01-14 23:47:21 +0200

    commit n

--- sample project 4/frontend/src/views/Secret.vue
+++ sample project 4/frontend/src/views/Secret.vue
@@ -2,9 +2,9 @@
 <template>
     <div class="container">
         <iframe 
-            width="100%" 
-            height="20%" 
-            src="https://app.powerbi.com/reportEmbed?reportId=d9540b6c-d359-4f5f-b736-a37eb63c0db8&autoAuth=true&ctid=980ebcd0-4d97-424f-9c8c-b506f698c330&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXNvdXRoLWFmcmljYS1ub3J0aC1hLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D" 
+            width="1920" 
+            height="1080" 
+            src="http://localhost:5300/" 
             frameborder="0" allowFullScreen="true">
         </iframe>
     </div>
